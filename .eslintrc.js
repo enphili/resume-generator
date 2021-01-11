4cdedc3aea76ef0110dc65fe1c0272661b5d9626
Roman <blurbinfacebook@yandex.ru>
2021-01-12 02:41:31 +0500

    init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,22 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'arrow-parens': 'off',
+    'comma-dangle': 'off',
+    'require-jsdoc': 'off',
+    'eol-last': 'off',
+    'linebreak-style': ['error', 'unix'],
+    'space-before-function-paren': ['error', 'never'],
+    'operator-linebreak': [2, 'before'],
+    'max-len': [
+      'error', {
+        'code': 120,
+        'ignoreComments': true,
+        'ignoreTrailingComments': true,
+        'ignoreUrls': true,
+        'ignoreStrings': true
+      }
+    ]
   }
 }
